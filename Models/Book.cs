9b5b0734f6fe2a4d58a7e06c1c0eceeb3dffc429
kbruner4 <90485716+kbruner4@users.noreply.github.com>
2022-02-16 08:33:45 -0700

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+
+//I updated this page so the data types match better
+namespace BookstoreProject.Models
+{
+    public partial class Book
+    {
+        [Key]
+        [Required]
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public string Isbn { get; set; }
+        public string Classification { get; set; }
+        public string Category { get; set; }
+        public int PageCount { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ Models/EFBookstoreProjectRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookstoreProject.Models
+{
+    public class EFBookstoreProjectRepository : iBookstoreProjectRepository
+    {
+        private BookstoreContext context { get; set; }
+
+        public EFBookstoreProjectRepository (BookstoreContext temp)
+        {
+            context = temp;
+        }
+        public IQueryable<Book> Books => context.Books;
+    }
+}
