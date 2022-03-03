668d04b8f23739e6f92894613f017569d70f0eaf
kbruner4 <90485716+kbruner4@users.noreply.github.com>
2022-03-02 22:54:16 -0700

    mission 9

---
+++ Controllers/CheckoutController.cs
@@ -0,0 +1,48 @@
+﻿using BookstoreProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookstoreProject.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        private ICheckoutRepository repo { get; set; }
+        private Basket basket { get; set; }
+
+        public CheckoutController (ICheckoutRepository temp, Basket b)
+        {
+            repo = temp;
+            basket = b;
+
+        }
+        [HttpGet]
+        public IActionResult Checkout()
+        {
+            return View(new Checkout());
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Checkout checkout)
+        {
+            if (basket.Items.Count() == 0)
+            {
+                ModelState.AddModelError("", "Sorry your basket is empty!");
+            }
+            if (ModelState.IsValid)
+            {
+                checkout.Lines = basket.Items.ToArray();
+                repo.SaveCheckout(checkout);
+                basket.ClearBasket();
+
+                return RedirectToPage("/CheckoutComplete");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
--- Models/Basket.cs
+++ Models/Basket.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BookstoreProject.Models
 {
-    public class Cart
+    public class Basket
     {
         public List<BasketLineItem> Items { get; set; } = new List<BasketLineItem>();
 
-        public void AddItem (Book book, int qty)
+        public virtual void AddItem (Book book, int qty)
         {
             BasketLineItem line = Items
                 .Where(b => b.Book.BookId == book.BookId)
@@ -28,6 +29,16 @@ namespace BookstoreProject.Models
                 line.Quantity += qty;
             }
         }
+
+        public virtual void RemoveItem(Book book)
+        {
+            Items.RemoveAll(x => x.Book.BookId == book.BookId);
+        }
+
+        public virtual void ClearBasket()
+        {
+            Items.Clear();
+        }
         public double CalculateTotal()
         {
             double sum = Items.Sum(x => x.Quantity * 25);
@@ -38,6 +49,7 @@ namespace BookstoreProject.Models
 
     public class BasketLineItem
     {
+        [Key]
         public int LineID { get; set; }
         public Book Book { get; set; }
         public int Quantity { get; set; }
